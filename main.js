859012b86b1b063516ebe4d2e971ac8e1a14ec84
brahim <brahimgb1team@gmail.com>
2021-10-18 20:16:55 +0000

    initial commit

--- main.js
+++ main.js
@@ -1,25 +1,64 @@
-const newBooks = [];
-function Books(title, author) {
-        this.title = title,
-        this.author = author
-    }
+const container = document.querySelector('.container');
+const addButton = document.querySelector('.add-btn');
+const form = document.getElementById('form');
 
-const book1 = new Books('Hermann Hesse', 'SteppenWolf');
-const book2 = new Books('Kafka', 'The Metamorphosis');
-const book3 = new Books('Miguel de Unamuno', 'Mist');
-newBooks.push(book1);
-newBooks.push(book2);
-newBooks.push(book3);
-console.log(newBooks)
+const book = new Book('Da Vinci Code', "Dan Brown");
+const book1 = new Book('Black Box Thinking', "Matthew Syed");
+const book2 = new Book('Climate Change', "Bill Gates");
 
-function addNewBook(book) {
-    
+const myBooks = [];
+myBooks.push(book);
+myBooks.push(book1);
+myBooks.push(book2);
+
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+}
+
+function addBookToLibrary(book) {
+  const bookDiv = document.createElement('div');
+  bookDiv.classList.add('bookInfos');
+  const h2 = document.createElement('h2');
+  const h3 = document.createElement('h3');
+  const removeBtn = document.createElement('button');
+  removeBtn.classList.add('remove');
+  h2.textContent = book.title;
+  h2.classList.add('book-title');
+  h3.textContent = book.author;
+  h3.classList.add('book-author');
+  removeBtn.textContent = "Remove";
+  removeBtn.classList.add('btn-1');
+  bookDiv.appendChild(h2);
+  bookDiv.appendChild(h3);
+  bookDiv.appendChild(removeBtn);
+  container.appendChild(bookDiv);
+}
+function displayBooks() {
+  myBooks.forEach(element => {
+    addBookToLibrary(element);
+  })
 }
 
 function removeBook(book) {
-
+  return myBooks.filter(b => b !== book);
 }
 
-function displayAllBooks() {
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.querySelector('.input-title').value;
+  const author = document.querySelector('.input-author').value;
+  const book = new Book(title, author);
+  myBooks.push(book);
+  addBookToLibrary(book);
+});
 
-}
+document.addEventListener('DOMContentLoaded', displayBooks);
+container.addEventListener('click',(r) => {
+    if(r.target.classList.contains('remove')){
+    r.target.parentElement.remove();
+    }
+    myBooks.filter((b) => {
+        r.target.textContent !== b.title
+    })
+});
