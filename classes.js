377ceff1fb0dde59cfb7e60231b98d87713d1fcf
Meri <mgogi13@freeuni.edu.ge>
2021-10-19 19:56:48 +0400

    Add Javascript Classes

---
+++ classes.js
@@ -0,0 +1,90 @@
+/* eslint-disable max-classes-per-file */
+
+class Book {
+    constructor(title, author, id) {
+      this.title = title;
+      this.author = author;
+      this.id = id;
+    }
+  }
+  
+  class Store {
+    static getBooks() {
+      let books;
+      if (localStorage.getItem('books') === null) {
+        books = [];
+      } else {
+        books = JSON.parse(localStorage.getItem('books'));
+      }
+      return books;
+    }
+  
+    static addBook(book) {
+      const books = Store.getBooks();
+      books.concat(book);
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  
+    static removeBook(id) {
+      const books = Store.getBooks();
+      books.forEach((book, index) => {
+        if (books.id === id) {
+          books.splice(index, 1);
+        }
+      });
+  
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  }
+
+class Display {
+    static displayBooks() {
+        const books = Store.getBooks();
+        books.forEach((book) => Display.addBookToList(book));
+    }
+    static addBookToList(book) {
+        const container = document.querySelector('.container');
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <p class="title">${book.title}</p>
+      <p class="author">${book.author}</p>
+      <button class="remove"}>Remove</button>`;
+      container.appendChild(div);
+    };
+
+    static deleteBook(el) {
+      if(el.classList.contains('remove')){
+        el.parentElement.remove();
+      }
+    }
+
+    static clearFields() {
+      document.querySelector('.input-title').value = '';
+      document.querySelector('.input-author').value = '';
+    }
+}
+
+// display books
+document.addEventListener('DOMContentLoaded', Display.displayBooks);
+document.querySelector('#form').addEventListener('submit', (e) => {
+  // get form values
+  e.preventDefault();
+  const title = document.querySelector('.input-title').value;
+  const author = document.querySelector('.input-author').value;
+  const id = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+
+  const book = new Book(title, author, id);
+  // console.log(book)
+  Display.addBookToList(book);
+
+  Store.addBook(book);
+
+  Display.clearFields();
+});
+
+// remove book list
+
+document.querySelector('.container').addEventListener('click', (e) => {
+  Display.deleteBook(e.target);
+  Store.removeBook(e.target.parentElement.id);
+});
